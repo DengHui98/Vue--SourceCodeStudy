5eebe2c482312f20e031f069dd9316127c5fdf00
DengHui98 <rdh1776222611@163.com>
2025-08-10 23:42:49 +0800

    环境提交

---
+++ packages/reactivity/src/index.ts
@@ -0,0 +1,8 @@
+import { isObject } from "@vue/shared";
+
+console.log(111111);
+const a = () => {
+  console.log(2222);
+};
+a();
+isObject({ a: 1 });
--- scripts/dev.js
+++ scripts/dev.js
@@ -1,5 +1,10 @@
 // 打包开发环境
 import { parseArgs } from "node:util";
+import { resolve } from "node:path";
+import { fileURLToPath } from "node:url";
+import { context } from "esbuild";
+import { createRequire } from "node:module";
+const __dirname = resolve(fileURLToPath(import.meta.url), "../");
 const {
   values: { format },
   positionals,
@@ -13,5 +18,21 @@ const {
     },
   },
 });
+const require = createRequire(import.meta.url);
+const target = positionals[0] || "vue3";
+const entry = resolve(__dirname, `../packages/${target}/src/index.ts`);
+const outfile = resolve(__dirname, `../packages/${target}/dist/${target}.${format}.js`);
+const pkg = require(`../packages/${target}/package.json`);
+console.log(pkg);
+
+context({
+  entryPoints: [entry],
+  outfile,
+  format,
+  platform: format === "cjs" ? "node" : "browser",
+  bundle: true,
+  sourcemap: true,
+  globalName: pkg.buildOptions?.name,
+}).then((ctx) => ctx.watch());
 
 console.log(format, positionals);
